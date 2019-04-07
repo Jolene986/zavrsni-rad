b4ea8db7300dfc23501bd8a48b676bd17847c70f
Jolene986 <jolene86@gmail.com>
2019-04-07 19:15:05 +0100

    event hendleri rade

--- src/components/ponudjeniOdgovori/ponudjeniOdgovor/PodgovorCheckbox.js
+++ src/components/ponudjeniOdgovori/ponudjeniOdgovor/PodgovorCheckbox.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react'
 
-
-
 export default class PodgovorCheckbox extends Component {
-
-  onChangeHandler = (event)=> {
-    this.props.provera(event.target.value);
+  onChangeHendler = (event)=> {
+    this.props.provera(event.target.value)
   }
   render() {
     return (
-      <label >
-    <input type="checkbox" value ={this.props.odgovor} onChange={this.onChangeHandler}/> 
-    {this.props.odgovor}
-    </label>
+      <label>
+        <input type="checkbox" value = {this.props.odgovor} onChange ={this.onChangeHendler}/>
+        {this.props.odgovor}
+      </label>
     )
   }
 }
