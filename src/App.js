af014e3dc0446fd517d9d0c6e020d2fc7f3c78df
Jolene986 <jolene86@gmail.com>
2019-04-14 22:48:33 +0100

    App state set

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Layout from './components/Layout';
 import Quiz from './containers/Quiz';
 
 class App extends Component {
+  state = {
+    tipKviza: '',
+    nadimak: '',
+    bodovi: 0
+  }
   render() {
     return (
       <div className="App">
